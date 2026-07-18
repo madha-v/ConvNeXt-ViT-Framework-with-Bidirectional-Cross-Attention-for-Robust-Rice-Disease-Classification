0ced6f2900e4007200a1bde02fb14b77958232db
madhav joshi <150261093+madha-v@users.noreply.github.com>
2026-07-18 22:32:17 +0530

    Fix formatting in return statement of augmentations.py

--- data/augmentations.py
+++ data/augmentations.py
@@ -21,4 +21,4 @@ def get_transforms(img_size=224):
         A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
         ToTensorV2(),
     ])
-    return train_transform, val_transform
+    return train_transform,  val_transform
